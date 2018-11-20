e61261631cd3d20c8f726d7797d9a0396dde1dc9
CoconutMacaroon <45187468+CoconutMacaroon@users.noreply.github.com>
2018-11-19 19:35:27 -0800

    Update main.py to optionally not quit
    When flag -w is present (for example 'python main.py -w'), then the app will wait for a keypress before quitting.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 # note this project is Python 3
 from random import randint
+from sys import argv
 
 # these values are inclusive
 min = int(input("Minimum bound: "))
 max = int(input("Maximum bound: "))
 print(randint(min, max))
+
+if "-w" in argv[1:]:
+	input("Press any key to exit")
