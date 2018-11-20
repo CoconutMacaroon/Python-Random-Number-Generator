d1ffb23556bdb50dd8cafe04c93da81367647d55
CoconutMacaroon <45187468+CoconutMacaroon@users.noreply.github.com>
2018-11-19 19:20:07 -0800

    Create main.py
    This is the main file. This file should be run to start the program. This file needs the Python random module.

---
+++ main.py
@@ -0,0 +1,7 @@
+# note this project is Python 3
+from random import randint
+
+# these values are inclusive
+min = int(input("Minimum bound: "))
+max = int(input("Maximum bound: "))
+print(randint(min, max))
